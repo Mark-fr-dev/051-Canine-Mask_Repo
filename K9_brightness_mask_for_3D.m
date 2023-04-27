f9c6ddb01aba40708c560c82e6179b7a66017105
Mark <mfrdevilliers@gmail.com>
2023-04-27 17:11:29 +0800

    Working B-mode brightness mask

--- K9_brightness_mask_for_3D.m
+++ K9_brightness_mask_for_3D.m
@@ -8,11 +8,21 @@ clear
 frame_num = 1;
 ele_num=23;
 
+vid_file = sprintf('B_mode_brightness_masks_%s.avi', datestr(now, 'yyyy-mm-dd_HHMMSS'));
+video_flag=1;
+if(video_flag==1)
+    vv= VideoWriter(vid_file);
+    vv.FrameRate=2;
+    open(vv)
+end
+
 file_dir= 'D:\Canine_study\051_Masks_repo\Output_data\';
 file_name = [file_dir 'BL_bmode_frame' num2str(frame_num) '_ele' num2str(ele_num)];
 load(file_name)
 
 [Na, Nl]=size(gray_data);
+
+for TH=20:50
 
 %Display B-Mode image
 figure(1);
@@ -30,7 +40,7 @@ strat_l=13; end_l = 51;
 
 filtered_gray = medfilt2(gray_data,[34 7]);
 mask =zeros(Na,Nl);
-TH = 35;  %B-Mode threshold brightness value 0-255
+%TH = 35;  %B-Mode threshold brightness value 0-255
 % TH_index = (gray_data>TH);
 TH_index = (filtered_gray>TH);
 mask(TH_index)=1;
@@ -49,6 +59,18 @@ clim([0 1])
 title(['Mask BL Bmode fr' num2str(frame_num) ' ele' num2str(ele_num) ' TH' ...
     num2str(TH)])
 
+
+if(video_flag ==1)
+        frame = getframe(gcf);
+        writeVideo(vv,frame)
+end
+
+end
+
+
+if(video_flag ==1)
+    close(vv)
+end
 
 % Edge detection
 % BW1 = edge(mask,'sobel');
