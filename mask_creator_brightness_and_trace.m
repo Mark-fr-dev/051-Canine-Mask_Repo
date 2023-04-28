297f96cdd1ef60b785fd1c7e4d6cd539d87dba6d
Mark <mfrdevilliers@gmail.com>
2023-04-28 14:27:08 +0800

    Update to file definition

--- mask_creator_brightness_and_trace.m
+++ mask_creator_brightness_and_trace.m
@@ -1,10 +1,21 @@
+% Code: B-Mode brightness -> mask
+% Input: B-mode axial x lateral matrix 
+% Output: ROI mask based on B-Mode brightness
+% Purpose: To produce a smooth 3D mask for K9 data display
+% 
+
 clear
+
+
+
+for 
 
 load d:\canine_study\051_Masks\bright_mask_BL_ele23_th30
 
 mask_bright =mask(1:end-100,:);
 load D:\Canine_study\051_Masks\00_BL0_Masks\BLmaskSAX_ele23_1
 mask_hand =mask;
+
 
 figure(21)
 subplot(1,3,1)
